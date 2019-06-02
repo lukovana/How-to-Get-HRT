0c6e863bbba2585cf1031f55dcb6dd6857c48cfa
lukovana <astaffers@gmail.com>
2019-06-02 02:06:03 -0700

    Js

--- scripts/search.php
+++ scripts/search.php
@@ -2,7 +2,7 @@
 
 function search($back) {
     echo
-    '<a href="#"><div id="mobiledrop" onclick="dropdown()" >
+    '<a href="#"><div id="mobiledrop" onclick="dropdown()" ondblclick="dropout()">
         <img src="', $back,'assets/images/hamburger.png" width="25" height="25" alt="hamburger" id="hamburger" />
     </div></a>
     <form>
