fff17e04ed49ec93a69090fdc92ec3038390d0e4
lukovana <astaffers@gmail.com>
2019-06-02 04:02:53 -0700

    CSS

--- scripts/search.php
+++ scripts/search.php
@@ -6,9 +6,9 @@ function search($back) {
         <img src="', $back,'assets/images/hamburger.png" width="25" height="25" alt="hamburger" id="hamburger" />
     </div></a>
     <form>
-        <div id="search">
+        <div id="searchbar">
+            <img src="', $back,'assets/images/search.png" width="25" height="25" id="searchimg"/>
             <input class="searchbox" type="text" placeholder="Search" aria-label="Search">
-            <img src="', $back,'assets/images/search.png" width="25"/>
         </div>
     </form>';
     };
