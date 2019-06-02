3fa0d91a99ba18391790c8b022c79b63d3e8dacb
lukovana <astaffers@gmail.com>
2019-06-02 02:33:42 -0700

    Js

--- scripts/mobiledrop.js
+++ scripts/mobiledrop.js
@@ -1,13 +1,17 @@
 var button = document.getElementById("mobiledrop");
 var navigation = document.getElementById("mobiledropdown");
+var display = document.getElementById("front");
 var count = 0;
 
 function dropdown() {
     count += 1;
-
-    navigation.style.display = "block;";
+    display.innerHTML = "This is count ", count;
 
     if (count % 2 == 0) {
         navigation.style.display = "none;";
+        display.style.backgroundColor = "#ff0000;"
+    } else {
+        navigation.style.display = "block;";
+        display.style.backgroundColor = "#00ff00;"
     }
 }
