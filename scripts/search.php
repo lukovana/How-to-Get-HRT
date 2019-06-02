a3680e39653fb64cfba1e667027fd964b0b394df
lukovana <astaffers@gmail.com>
2019-06-02 04:50:53 -0700

    Searchbar

--- scripts/search.php
+++ scripts/search.php
@@ -5,11 +5,9 @@ function search($back) {
     '<a href="#"><div id="mobiledrop" onclick="dropdown()">
         <img src="', $back,'assets/images/hamburger.png" width="25" height="25" alt="hamburger" id="hamburger" />
     </div></a>
-    <form>
-        <div id="searchbar">
-            <img src="', $back,'assets/images/search.png" width="25" height="25" id="searchimg"/>
-            <input class="searchbox" type="text" placeholder="Search" aria-label="Search">
-        </div>
+    <form id="searchbar">
+        <img src="', $back,'assets/images/search.png" width="25" height="25" id="searchimg"/>
+        <input id="searchbox" type="text" placeholder="Search">
     </form>';
     };
 ?>
