496dd09421f6ae4401bb8d6560d1557ec2d63757
lukovana <astaffers@gmail.com>
2019-06-02 02:48:53 -0700

    Js

--- scripts/mobiledrop.js
+++ scripts/mobiledrop.js
@@ -1,8 +1,19 @@
+var count = 0;
+
 function dropdown() {
     const navigation = document.getElementById("mobiledropdown");
-    navigation.style.display = "block";
+    count += 1;
+    console.log(count)
+    if (count % 2 == 0) {
+        console.log("entered % 2")
+        navigation.style.display = "none";
+    } else {
+        console.log("entered else")
+        navigation.style.display = "block";
+    }
 }
-function dropout() {
+/*function dropout() {
+    count += 1;
     const navigation = document.getElementById("mobiledropdown");
     navigation.style.display = "none";
-}
+}*/
