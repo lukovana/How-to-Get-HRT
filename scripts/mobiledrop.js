cb938075207d5e84290a286dc9625690056e4ccc
lukovana <astaffers@gmail.com>
2019-06-02 02:15:31 -0700

    Js

--- scripts/mobiledrop.js
+++ scripts/mobiledrop.js
@@ -1,7 +1,7 @@
 function dropdown() {
     const navigation = document.getElementById("mobiledropdown");
     const count = 0;
-    count++;
+    count += 1;
 
     if (count % 2 == 0) {
         navigation.style.display = "none";
