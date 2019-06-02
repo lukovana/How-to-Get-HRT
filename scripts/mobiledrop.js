04bbfb41a9d6db00c2cbe7f7a1a9b59299dc4a8e
lukovana <astaffers@gmail.com>
2019-06-02 01:49:42 -0700

    Js fix

--- scripts/mobiledrop.js
+++ scripts/mobiledrop.js
@@ -2,3 +2,7 @@ function dropdown() {
     const navigation = document.getElementById("mobiledropdown");
     navigation.style.display = "block";
 }
+function dropout() {
+    const navigation = document.getElementById("mobiledropdown");
+    navigation.style.display = "none";
+}
