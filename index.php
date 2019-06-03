d6905cc04cabdd291f1cdfd4f0917963ac31e9b3
lukovana <astaffers@gmail.com>
2019-06-03 18:20:07 -0400

    CSS

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" /> 
         <meta property="og:image" content="https://i.redd.it/wyhth24b4xc11.png" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="http://www.howtogethrt.com" />
