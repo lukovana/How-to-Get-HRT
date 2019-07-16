2449973fd87301c682a745e77dcb593039eb9043
Lab Tech <LabTech@cse.local>
2019-07-16 19:13:11 -0400

    psych styles

--- pages/states/CA/psychs.php
+++ pages/states/CA/psychs.php
@@ -7,9 +7,8 @@
         <title>How to Get HRT</title>
 
 		<link rel="stylesheet" href="../../../stylesheets/website.css"> 
-		<link rel="stylesheet" href="../../../stylesheets/main.css">
+		<link rel="stylesheet" href="../../../stylesheets/database.css">
         <script src="../../../scripts/website.js"></script>
-        <link rel="stylesheet" href="../../../stylesheets/doc.css">
     </head>
     <body>
 		<div class="wrapper" id="h_wrapper">
@@ -48,32 +47,36 @@
 				<p>Select Your County!</p>
 
 				<!-- Enters psychologist information from the database -->
-				<?php 	include '../scripts/database.php';
-						include 'scripts/enter_data.php';
-				?>
+				<section id="allpsychs">
+					<?php 	include '../scripts/database.php';
+							include 'scripts/enter_data.php';
+					?>
+				</section>
 
-				<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" id="form1">
-					
-					<?php 	include 'scripts/counties_select.php';
-							#include '../scripts/formselect.php'
-							#select("county", $counties);
-							#select("insurance", $insurances);?>
+				<section id="allforms">
+					<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" id="form1">
+						
+						<?php 	include 'scripts/counties_select.php';
+								#include '../scripts/formselect.php'
+								#select("county", $counties);
+								#select("insurance", $insurances);?>
 
-					<button type="Submit">Submit</button>
-					
-				</form>
+						<button type="Submit">Submit</button>
+						
+					</form>
 
-				<?php	include '../scripts/database.php';
+					<?php	include '../scripts/database.php';
 
-						//  Take in the county data from the form.
-						$county = $_POST['county']; 
+							//  Take in the county data from the form.
+							$county = $_POST['county']; 
 
-						echo 
-						'<form method="POST" action="process.php" id="form2">';
-						include 'scripts/insurances_select.php';
-						getInsuranceList('California', $county);
-						echo '<button type="Submit">Submit</button></form>';
-				?>
+							echo 
+							'<form method="POST" action="process.php" id="form2">';
+							include 'scripts/insurances_select.php';
+							getInsuranceList('California', $county);
+							echo '<button type="Submit">Submit</button></form>';
+					?>
+				</section>
 			</section>
 		</main>
 		<?php include '../../../templates/footer.php';?>
